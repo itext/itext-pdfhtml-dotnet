22c7f61b27fcff214b51382c13d6620b087dd643
Alexey Subach <alexey.subach@duallab.com>
2016-12-06 13:55:00 +0300

    Improve log message testing
    Autoported commit.
    Original commit hash: [43382bf0]

--- itext.tests/itext.html2pdf.tests/itext/html2pdf/css/TextPropertiesTest.cs
+++ itext.tests/itext.html2pdf.tests/itext/html2pdf/css/TextPropertiesTest.cs
@@ -77,7 +77,7 @@ namespace iText.Html2pdf.Css {
         /// <exception cref="System.IO.IOException"/>
         /// <exception cref="System.Exception"/>
         [NUnit.Framework.Test]
-        [LogMessage("text-decoration: blink not supported")]
+        [LogMessage(iText.Html2pdf.LogMessageConstant.TEXT_DECORATION_BLINK_NOT_SUPPORTED)]
         public virtual void TextDecoration01Test() {
             HtmlConverter.ConvertToPdf(new FileInfo(sourceFolder + "textDecorationTest01.html"), new FileInfo(destinationFolder
                  + "textDecorationTest01.pdf"));
@@ -88,7 +88,7 @@ namespace iText.Html2pdf.Css {
         /// <exception cref="System.IO.IOException"/>
         /// <exception cref="System.Exception"/>
         [NUnit.Framework.Test]
-        [LogMessage("text-indent in percents is not supported")]
+        [LogMessage(iText.Html2pdf.LogMessageConstant.TEXT_INDENT_IN_PERCENTS_IS_NOT_SUPPORTED)]
         public virtual void TextIndent01Test() {
             HtmlConverter.ConvertToPdf(new FileInfo(sourceFolder + "textIndentTest01.html"), new FileInfo(destinationFolder
                  + "textIndentTest01.pdf"));
