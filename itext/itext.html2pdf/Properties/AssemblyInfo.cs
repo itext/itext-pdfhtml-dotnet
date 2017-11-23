acc41ea8730fbaf0551cf821996ea4af1e0f4e9d
Ilya Idamkin <ilya.idamkin@duallab.com>
2017-11-23 19:56:10 +0300

    Add missing licensekey version attribute

--- itext/itext.html2pdf/Properties/AssemblyInfo.cs
+++ itext/itext.html2pdf/Properties/AssemblyInfo.cs
@@ -24,3 +24,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: AssemblyInformationalVersion("1.1.0-SNAPSHOT")]
+
+[assembly: Versions.Attributes.KeyVersion("3.0.0.0")]
