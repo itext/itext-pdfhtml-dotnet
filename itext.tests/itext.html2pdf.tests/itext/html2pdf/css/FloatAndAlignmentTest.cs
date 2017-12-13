40e2efcf688724fdcb41367126e43ee3d85e66f7
Veronika Lisovskaya <veronika.lisovskaya@duallab.com>
2017-12-13 15:42:14 +0300

    Test class added: com.itextpdf.html2pdf.FloatAndAlignmentTest
    DEVSIX-1268
    
    Autoported commit.
    Original commit hash: [bcfcdc41]

---
+++ itext.tests/itext.html2pdf.tests/itext/html2pdf/css/FloatAndAlignmentTest.cs
@@ -0,0 +1,96 @@
+using System;
+using System.IO;
+using iText.Html2pdf;
+using iText.Kernel.Utils;
+using iText.Test;
+using iText.Test.Attributes;
+
+namespace iText.Html2pdf.Css {
+    public class FloatAndAlignmentTest : ExtendedITextTest {
+        public static readonly String sourceFolder = iText.Test.TestUtil.GetParentProjectDirectory(NUnit.Framework.TestContext
+            .CurrentContext.TestDirectory) + "/resources/itext/html2pdf/css/FloatAndAlignmentTest/";
+
+        public static readonly String destinationFolder = NUnit.Framework.TestContext.CurrentContext.TestDirectory
+             + "/test/itext/html2pdf/css/FloatAndAlignmentTest/";
+
+        [NUnit.Framework.OneTimeSetUp]
+        public static void BeforeClass() {
+            CreateOrClearDestinationFolder(destinationFolder);
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        [LogMessage(iText.Html2pdf.LogMessageConstant.INVALID_CSS_PROPERTY_DECLARATION, Count = 2)]
+        public virtual void SingleBlockSingleParagraphRight() {
+            /* this test shows different combinations of float values blocks and  paragraph align RIGHT within div container
+            */
+            //TODO: update test after ticket DEVSIX-1720  fix (WARN Invalid css property declaration: float: initial)
+            //TODO: update cmp file after ticket DEVSIX-1268 fix (Float property...)
+            RunTest("SingleBlockSingleParagraphRight", "diffRight01_");
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        [LogMessage(iText.Html2pdf.LogMessageConstant.INVALID_CSS_PROPERTY_DECLARATION, Count = 2)]
+        public virtual void SingleBlockSingleParagraphLeft() {
+            //TODO: update test after ticket DEVSIX-1720  fix (WARN Invalid css property declaration: float: initial)
+            //TODO: update cmp file after ticket DEVSIX-1268 fix (Float property...)
+            RunTest("SingleBlockSingleParagraphLeft", "diffLeft01_");
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        [LogMessage(iText.Html2pdf.LogMessageConstant.INVALID_CSS_PROPERTY_DECLARATION, Count = 2)]
+        public virtual void SingleBlockSingleParagraphJustify() {
+            //TODO: update test after ticket DEVSIX-1720  fix (WARN Invalid css property declaration: float: initial)
+            //TODO: update cmp file after ticket DEVSIX-1268 fix (Float property...)
+            RunTest("SingleBlockSingleParagraphJustify", "diffJust01_");
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        [LogMessage(iText.Html2pdf.LogMessageConstant.INVALID_CSS_PROPERTY_DECLARATION, Count = 2)]
+        public virtual void SingleBlockSingleParagraphCenter() {
+            //TODO: update test after ticket DEVSIX-1720  fix (WARN Invalid css property declaration: float: initial)
+            //TODO: update cmp file after ticket DEVSIX-1268 fix (Float property...)
+            RunTest("SingleBlockSingleParagraphCenter", "diffCent01_");
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        public virtual void SeveralBlocksSingleParagraph() {
+            /* this test shows different combinations of 3 float values blocks and 1 paragraph aligns within div container
+            */
+            //TODO: update cmp file after ticket DEVSIX-1268 fix (Float property...)
+            RunTest("SeveralBlocksSingleParagraph", "diffSev01_");
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        public virtual void BlocksInsideParagraph() {
+            /* this test shows different combinations of 3 float values blocks and 1 paragraph aligns within div container
+            * now it points not only incorrect alignment vs float positioning, but also incorrect float area
+            */
+            //TODO: update cmp file after ticket DEVSIX-1268 fix (Float property...)
+            //TODO: update after DEVSIX-1437 fix (Fix edge cases for floats splitting)
+            RunTest("BlocksInsideParagraph", "diffInside01_");
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        private void RunTest(String testName, String diff) {
+            String htmlName = sourceFolder + testName + ".html";
+            String outFileName = destinationFolder + testName + ".pdf";
+            String cmpFileName = sourceFolder + "cmp_" + testName + ".pdf";
+            HtmlConverter.ConvertToPdf(new FileInfo(htmlName), new FileInfo(outFileName));
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(outFileName, cmpFileName, destinationFolder
+                , diff));
+        }
+    }
+}
