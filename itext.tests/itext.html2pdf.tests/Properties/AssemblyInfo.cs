e044f8569a6b0778f058387c4c10998f79a50e84
Pavel Alay <pavel.alay@duallab.com>
2018-01-20 17:21:56 +0300

    [RELEASE] 1.0.3-SNAPSHOT -> 1.0.3

--- itext.tests/itext.html2pdf.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.html2pdf.tests/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("iText Group NV")]
 [assembly: AssemblyProduct("iText")]
-[assembly: AssemblyCopyright("Copyright (c) 1998-2017 iText Group NV")]
+[assembly: AssemblyCopyright("Copyright (c) 1998-2018 iText Group NV")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.3.0")]
 [assembly: AssemblyFileVersion("1.0.3.0")]
-[assembly: AssemblyInformationalVersion("1.0.3-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("1.0.3")]
 
 #if !NETSTANDARD1_6
 [assembly: NUnit.Framework.Timeout(300000)]
