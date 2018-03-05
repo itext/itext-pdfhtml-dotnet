c70d2c5e4d30e941ba87919d934ba37f792b51cf
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2018-03-05 10:49:19 +0300

    Disable collapsing margins of page margin box with it's content
    Running elements might have margins and therefore might collapse margins
    with page margin box. It's unclear whether this should happen, however
    right now this is undesired due to techinical limitations: we set fixed
    height of page marign box in order to handle it's content overflowing,
    however this fixed is calculated based on page margin box margins. If
    collapsing of margins is enabled, actual page margin box margin value is
    unknown before layoting.
    
    DEVSIX-1437
    
    Autoported commit.
    Original commit hash: [9001eb74]

--- itext/itext.html2pdf/itext/html2pdf/attach/impl/tags/PageMarginBoxWorker.cs
+++ itext/itext.html2pdf/itext/html2pdf/attach/impl/tags/PageMarginBoxWorker.cs
@@ -1,11 +1,16 @@
 using iText.Html2pdf.Attach;
 using iText.Html2pdf.Html.Node;
 using iText.Kernel.Pdf.Tagging;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
 using iText.Layout.Tagging;
 
 namespace iText.Html2pdf.Attach.Impl.Tags {
     /// <summary>TagWorker class for the page margin box.</summary>
     public class PageMarginBoxWorker : DivTagWorker {
+        private Div wrappedElementResult;
+
         /// <summary>
         /// Creates a new
         /// <see cref="PageMarginBoxWorker"/>
@@ -19,9 +24,18 @@ namespace iText.Html2pdf.Attach.Impl.Tags {
 
         public override void ProcessEnd(IElementNode element, ProcessorContext context) {
             base.ProcessEnd(element, context);
+            GetElementResult().SetProperty(Property.COLLAPSING_MARGINS, true);
+            if (base.GetElementResult() is IBlockElement) {
+                wrappedElementResult = new Div().Add((IBlockElement)base.GetElementResult());
+                wrappedElementResult.SetProperty(Property.COLLAPSING_MARGINS, false);
+            }
             if (GetElementResult() is IAccessibleElement) {
                 ((IAccessibleElement)GetElementResult()).GetAccessibilityProperties().SetRole(StandardRoles.ARTIFACT);
             }
+        }
+
+        public override IPropertyContainer GetElementResult() {
+            return wrappedElementResult == null ? base.GetElementResult() : wrappedElementResult;
         }
     }
 }
