465018bc418447ead3a2a9600352e757cb5ca7cf
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2018-11-26 06:58:36 +0100

    Take line indent in account during intrinsic widths calculations only for the first child in the line
    DEVSIX-1896
    
    Autoported commit.
    Original commit hash: [6426862b]

--- itext.tests/itext.html2pdf.tests/itext/html2pdf/css/w3c/css21/text/TextIndentIntrinsic001Test.cs
+++ itext.tests/itext.html2pdf.tests/itext/html2pdf/css/w3c/css21/text/TextIndentIntrinsic001Test.cs
@@ -3,7 +3,6 @@ using iText.Html2pdf.Css.W3c;
 
 namespace iText.Html2pdf.Css.W3c.Css21.Text {
     public class TextIndentIntrinsic001Test : W3CCssAhemFontTest {
-        // TODO 5th and 6th blocks min width is still different from what's in browsers. -- their min-width is bigger than required.
         protected internal override String GetHtmlFileName() {
             return "text-indent-intrinsic-001.xht";
         }
