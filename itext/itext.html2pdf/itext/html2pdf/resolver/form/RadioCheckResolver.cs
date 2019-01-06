4628511df5410bb99f4c170ac365527be8821e2a
iText Software <development@itextpdf.com>
2019-01-06 22:50:46 +0300

    Add missing copyright headers
    Autoported commit.
    Original commit hash: [b9873023]
    Manual files:
    src/main/java/com/itextpdf/html2pdf/HtmlConverter.java
    src/main/java/com/itextpdf/html2pdf/attach/impl/DefaultHtmlProcessor.java
    src/main/java/com/itextpdf/html2pdf/attach/impl/layout/form/renderer/AbstractFormFieldRenderer.java
    src/main/java/com/itextpdf/html2pdf/css/resolve/CssContentElementNode.java
    src/test/java/com/itextpdf/html2pdf/ResourceResolverTest.java
    src/test/java/com/itextpdf/html2pdf/css/CssStyleSheetParserTest.java

--- itext/itext.html2pdf/itext/html2pdf/resolver/form/RadioCheckResolver.cs
+++ itext/itext.html2pdf/itext/html2pdf/resolver/form/RadioCheckResolver.cs
@@ -19,7 +19,7 @@ namespace iText.Html2pdf.Resolver.Form {
 
         /*
         This file is part of the iText (R) project.
-        Copyright (c) 1998-2018 iText Group NV
+        Copyright (c) 1998-2019 iText Group NV
         Authors: Bruno Lowagie, Paulo Soares, et al.
         
         This program is free software; you can redistribute it and/or modify
