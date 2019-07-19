e479b4baeae5d8813dcb4e27e1d411f295001388
Kate Ivanova <ekaterina.ivanova@duallab.com>
2019-07-19 09:15:05 +0200

    Move tests from FT repository
    DEVSIX-3038
    
    Autoported commit.
    Original commit hash: [45c2a1e3]

--- itext.tests/itext.html2pdf.tests/itext/html2pdf/css/PageRuleTest.cs
+++ itext.tests/itext.html2pdf.tests/itext/html2pdf/css/PageRuleTest.cs
@@ -623,6 +623,27 @@ namespace iText.Html2pdf.Css {
         /// <exception cref="System.IO.IOException"/>
         /// <exception cref="System.Exception"/>
         [NUnit.Framework.Test]
+        public virtual void CheckMarginBoxFixDimension() {
+            RunTest("checkMarginBoxFixDimension");
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        public virtual void CheckMarginBoxMaxMinDimension() {
+            RunTest("checkMarginBoxMaxMinDimension");
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
+        public virtual void CheckMarginBoxMarginPaddings() {
+            RunTest("checkMarginBoxMarginPaddings");
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Test]
         public virtual void MediaAppliedToRunningElementsProperties() {
             MediaDeviceDescription printMediaDevice = new MediaDeviceDescription("print");
             ConverterProperties converterProperties = new ConverterProperties().SetMediaDeviceDescription(printMediaDevice
