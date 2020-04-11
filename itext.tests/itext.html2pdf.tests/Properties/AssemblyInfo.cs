703535d5f7ea224bdedf4317cf589e5c09bb6ea8
iText Software <development@itextpdf.com>
2020-04-11 12:39:44 +0300

    [RELEASE] Update dependency versions

--- itext.tests/itext.html2pdf.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.html2pdf.tests/Properties/AssemblyInfo.cs
@@ -15,9 +15,9 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ae4e5743-0665-4705-9a33-07ea57cdd269")]
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-SNAPSHOT")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
+[assembly: AssemblyInformationalVersion("3.0.1-SNAPSHOT")]
 
 #if !NETSTANDARD1_6
 [assembly: NUnit.Framework.Timeout(300000)]
