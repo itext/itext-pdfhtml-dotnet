9ac7ad249a1f14054093c3c95a0789d35a2f5954
Alex Rasolko <alex.rasolko@duallab.com>
2020-08-21 13:58:27 +0000

    Add background properties tests group css2 - PART_2
    DEVSIX-4346
    
    Autoported commit.
    Original commit hash: [872b688c]

---
+++ itext.tests/itext.html2pdf.tests/itext/html2pdf/css/w3c/css21/backgrounds/Background194Test.cs
@@ -0,0 +1,12 @@
+using System;
+using iText.Html2pdf.Css.W3c;
+
+namespace iText.Html2pdf.Css.W3c.Css21.Backgrounds {
+    // TODO DEVSIX-1457. Background-position is not supported: currently we apply background from the top
+    //  and not from the bottom of the area
+    public class Background194Test : W3CCssTest {
+        protected internal override String GetHtmlFileName() {
+            return "background-194.xht";
+        }
+    }
+}
