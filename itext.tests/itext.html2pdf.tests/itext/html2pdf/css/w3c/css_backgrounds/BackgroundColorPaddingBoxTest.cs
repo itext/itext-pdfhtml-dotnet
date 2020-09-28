dd91a889c42f914729f468efe25fe39c6616a740
Alexey Subach <alexey.subach@itextpdf.com>
2020-09-28 08:32:52 +0000

    Remove obsolete TODO
    DEVSIX-4389
    
    Autoported commit.
    Original commit hash: [371b77e2]

--- itext.tests/itext.html2pdf.tests/itext/html2pdf/css/w3c/css_backgrounds/BackgroundColorPaddingBoxTest.cs
+++ itext.tests/itext.html2pdf.tests/itext/html2pdf/css/w3c/css_backgrounds/BackgroundColorPaddingBoxTest.cs
@@ -25,7 +25,6 @@ using iText.Html2pdf.Css.W3c;
 
 namespace iText.Html2pdf.Css.W3c.Css_backgrounds {
     // TODO DEVSIX-2105 support background-clip
-    // TODO DEVSIX-4389 support background-color
     public class BackgroundColorPaddingBoxTest : W3CCssTest {
         protected internal override String GetHtmlFileName() {
             return "background_color_padding_box.htm";
