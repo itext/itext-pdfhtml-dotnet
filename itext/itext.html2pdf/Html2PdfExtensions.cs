a27aa28812ffb5531315e5a676957fea090ecb5b
Eugene Bochilo <eugene.bochilo@duallab.com>
2020-11-16 23:28:25 +0300

    Add LinkedList.getLast method analog to extensions class
    DEVSIX-4693

--- itext/itext.html2pdf/Html2PdfExtensions.cs
+++ itext/itext.html2pdf/Html2PdfExtensions.cs
@@ -411,6 +411,10 @@ internal static class Html2PdfExtensions {
     public static T JGetFirst<T>(this LinkedList<T> list) {
         return list.First.Value;
     }
+    
+    public static T JGetLast<T>(this LinkedList<T> list) {
+        return list.Last.Value;
+    }
 
     public static void EnsureCapacity<T>(this List<T> list, int capacity) {
         if (capacity > list.Count) {
