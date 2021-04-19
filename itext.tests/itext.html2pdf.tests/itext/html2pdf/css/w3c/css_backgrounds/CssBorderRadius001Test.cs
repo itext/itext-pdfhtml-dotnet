bee907d0c741a6eed4f45607e62716019431aae5
Maksim Bezrukov <maksim.bezrukov@duallab.com>
2021-04-19 15:11:13 +0200

    Update cmp files.
    DEVSIX-2025
    
    Autoported commit.
    Original commit hash: [ce158b9c]

--- itext.tests/itext.html2pdf.tests/itext/html2pdf/css/w3c/css_backgrounds/CssBorderRadius001Test.cs
+++ itext.tests/itext.html2pdf.tests/itext/html2pdf/css/w3c/css_backgrounds/CssBorderRadius001Test.cs
@@ -24,7 +24,6 @@ using System;
 using iText.Html2pdf.Css.W3c;
 
 namespace iText.Html2pdf.Css.W3c.Css_backgrounds {
-    // TODO DEVSIX-2025 check that border-radius: 50% processed correctly and change cmp
     public class CssBorderRadius001Test : W3CCssTest {
         protected internal override String GetHtmlFileName() {
             return "css-border-radius-001.html";
