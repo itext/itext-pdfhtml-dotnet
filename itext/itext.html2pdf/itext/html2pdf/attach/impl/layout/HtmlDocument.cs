0c3146e419310f91f96520dd60171581659918a6
Alexey Subach <alexey.subach@itextpdf.com>
2021-06-07 13:01:23 +0000

    Move target counter handler preparation on relayot from HtmlDocument to Document
    DEVSIX-5502
    
    Autoported commit.
    Original commit hash: [65c57aee]

--- itext/itext.html2pdf/itext/html2pdf/attach/impl/layout/HtmlDocument.cs
+++ itext/itext.html2pdf/itext/html2pdf/attach/impl/layout/HtmlDocument.cs
@@ -59,7 +59,6 @@ namespace iText.Html2pdf.Attach.Impl.Layout {
         public override void Relayout() {
             if (rootRenderer is HtmlDocumentRenderer) {
                 ((HtmlDocumentRenderer)rootRenderer).RemoveEventHandlers();
-                ((HtmlDocumentRenderer)rootRenderer).GetTargetCounterHandler().PrepareHandlerToRelayout();
                 base.Relayout();
                 ((HtmlDocumentRenderer)rootRenderer).ProcessWaitingElement();
             }
