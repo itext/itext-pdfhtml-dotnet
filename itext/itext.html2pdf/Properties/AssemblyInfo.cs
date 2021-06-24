a29df1754008fbd6d8f43582ebb63659358be4cc
iText Software <development@itextpdf.com>
2021-06-24 19:10:39 +0300

    Update AssemblyInfo with the new licensekey version

--- itext/itext.html2pdf/Properties/AssemblyInfo.cs
+++ itext/itext.html2pdf/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using Versions.Attributes;
 [assembly: AssemblyFileVersion("3.0.6.0")]
 [assembly: AssemblyInformationalVersion("3.0.6-SNAPSHOT")]
 
-[assembly: Versions.Attributes.KeyVersion("3.1.4.0")]
+[assembly: Versions.Attributes.KeyVersion("3.1.5.0")]
