b978ea8e391c3170e0723960d73021fc328a2829
Eugene Bochilo <eugene.bochilo@duallab.com>
2021-09-20 19:17:22 +0300

    Remove KeyVersionAttribute class
    DEVSIX-5807

--- itext/itext.html2pdf/Properties/AssemblyInfo.cs
+++ itext/itext.html2pdf/Properties/AssemblyInfo.cs
@@ -31,5 +31,3 @@ using Versions.Attributes;
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
 [assembly: AssemblyInformationalVersion("4.0.0-SNAPSHOT")]
-
-[assembly: Versions.Attributes.KeyVersion("3.1.5.0")]
