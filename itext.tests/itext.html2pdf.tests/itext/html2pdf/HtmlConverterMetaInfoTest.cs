2f2977965077166a3b80911e08cdd9d5001cce7e
Vladislav Kiper <vladislav.kiper@duallab.com>
2021-09-23 14:44:31 +0000

    Add test demonstrating a problem with META_INFO not being propagated to elements
    DEVSIX-5790
    
    Autoported commit.
    Original commit hash: [69c3ffa5]

---
+++ itext.tests/itext.html2pdf.tests/itext/html2pdf/HtmlConverterMetaInfoTest.cs
@@ -0,0 +1,110 @@
+using System.IO;
+using NUnit.Framework;
+using iText.Commons.Actions.Contexts;
+using iText.Html2pdf.Attach;
+using iText.Html2pdf.Attach.Impl;
+using iText.Html2pdf.Attach.Impl.Tags;
+using iText.Html2pdf.Html;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Layout;
+using iText.Layout.Properties;
+using iText.Layout.Renderer;
+using iText.StyledXmlParser.Node;
+using iText.Test;
+
+namespace iText.Html2pdf {
+    public class HtmlConverterMetaInfoTest : ExtendedITextTest {
+        [NUnit.Framework.Test]
+        public virtual void TestMetaInfoShouldBePresent() {
+            DocumentProperties documentProperties = new DocumentProperties();
+            IMetaInfo o = new _IMetaInfo_34();
+            documentProperties.SetEventCountingMetaInfo(o);
+            ConverterProperties converterProperties = new ConverterProperties();
+            converterProperties.SetEventMetaInfo(o);
+            HtmlConverterMetaInfoTest.InvocationAssert invocationAssert = new HtmlConverterMetaInfoTest.InvocationAssert
+                ();
+            converterProperties.SetTagWorkerFactory(new HtmlConverterMetaInfoTest.AssertMetaInfoTagWorkerFactory(invocationAssert
+                ));
+            // TODO DEVSIX-5790 fix assertion error - all assertions must pass
+            NUnit.Framework.Assert.Catch(typeof(AssertionException), () => {
+                HtmlConverter.ConvertToDocument("<!DOCTYPE html>\n" + "<html>\n" + "\n" + "<body>\n" + "<div>\n" + "The content of the div\n"
+                     + "</div>\n" + "</body>\n" + "\n" + "</html>\n", new PdfDocument(new PdfWriter(new MemoryStream())), 
+                    converterProperties).Close();
+            }
+            );
+            // TODO DEVSIX-5790 #isInvoked status must be true
+            NUnit.Framework.Assert.IsFalse(invocationAssert.IsInvoked());
+        }
+
+        private sealed class _IMetaInfo_34 : IMetaInfo {
+            public _IMetaInfo_34() {
+            }
+        }
+
+        private class AssertMetaInfoTagWorkerFactory : DefaultTagWorkerFactory {
+            private readonly HtmlConverterMetaInfoTest.InvocationAssert invocationAssert;
+
+            private AssertMetaInfoTagWorkerFactory(HtmlConverterMetaInfoTest.InvocationAssert invocationAssert) {
+                this.invocationAssert = invocationAssert;
+            }
+
+            public override ITagWorker GetCustomTagWorker(IElementNode tag, ProcessorContext context) {
+                if (TagConstants.DIV.Equals(tag.Name())) {
+                    return new HtmlConverterMetaInfoTest.AssertMetaInfoDivTagWorker(tag, context, invocationAssert);
+                }
+                return base.GetCustomTagWorker(tag, context);
+            }
+        }
+
+        private class AssertMetaInfoDivTagWorker : DivTagWorker {
+            private readonly HtmlConverterMetaInfoTest.InvocationAssert invocationAssert;
+
+            public AssertMetaInfoDivTagWorker(IElementNode element, ProcessorContext context, HtmlConverterMetaInfoTest.InvocationAssert
+                 invocationAssert)
+                : base(element, context) {
+                this.invocationAssert = invocationAssert;
+            }
+
+            public override IPropertyContainer GetElementResult() {
+                Div result = (Div)base.GetElementResult();
+                result.SetNextRenderer(new HtmlConverterMetaInfoTest.AssertMetaInfoDivTagRenderer(result, invocationAssert
+                    ));
+                return result;
+            }
+        }
+
+        private class AssertMetaInfoDivTagRenderer : DivRenderer {
+            private readonly HtmlConverterMetaInfoTest.InvocationAssert invocationAssert;
+
+            public AssertMetaInfoDivTagRenderer(Div modelElement, HtmlConverterMetaInfoTest.InvocationAssert invocationAssert
+                )
+                : base(modelElement) {
+                this.invocationAssert = invocationAssert;
+            }
+
+            public override LayoutResult Layout(LayoutContext layoutContext) {
+                NUnit.Framework.Assert.IsNotNull(this.GetProperty(Property.META_INFO));
+                invocationAssert.SetInvoked(true);
+                return base.Layout(layoutContext);
+            }
+
+            public override IRenderer GetNextRenderer() {
+                return new HtmlConverterMetaInfoTest.AssertMetaInfoDivTagRenderer((Div)modelElement, invocationAssert);
+            }
+        }
+
+        private class InvocationAssert {
+            private bool invoked;
+
+            public virtual void SetInvoked(bool invoked) {
+                this.invoked = invoked;
+            }
+
+            public virtual bool IsInvoked() {
+                return invoked;
+            }
+        }
+    }
+}
