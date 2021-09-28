6485a9cc03081242f177875a209f9ce5dc8ccc9f
iText Software <development@itextpdf.com>
2021-09-28 14:26:39 +0000

    [AFTER RELEASE] pdfHTML 4.0.0

--- itext/itext.html2pdf/itext/html2pdf/actions/data/PdfHtmlProductData.cs
+++ itext/itext.html2pdf/itext/html2pdf/actions/data/PdfHtmlProductData.cs
@@ -33,7 +33,7 @@ namespace iText.Html2pdf.Actions.Data {
     public sealed class PdfHtmlProductData {
         private const String PDF_HTML_PUBLIC_PRODUCT_NAME = "pdfHTML";
 
-        private const String PDF_HTML_VERSION = "4.0.0-SNAPSHOT";
+        private const String PDF_HTML_VERSION = "4.0.1-SNAPSHOT";
 
         private const int PDF_HTML_COPYRIGHT_SINCE = 2000;
 
