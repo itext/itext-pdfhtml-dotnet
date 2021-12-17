be5e57f4f979942a2cda5ead647da36d3493cc85
iText Software <development@itextpdf.com>
2021-12-17 07:00:18 +0000

    [RELEASE] pdfHTML 4.0.1

--- itext/itext.html2pdf/Properties/AssemblyInfo.cs
+++ itext/itext.html2pdf/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using Versions.Attributes;
 
 [assembly: AssemblyVersion("4.0.1.0")]
 [assembly: AssemblyFileVersion("4.0.1.0")]
-[assembly: AssemblyInformationalVersion("4.0.1-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("4.0.1")]
