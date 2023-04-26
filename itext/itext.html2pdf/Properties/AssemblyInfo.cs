2e60b35dfb7b0bb6f8761215943376e64cc873e9
iText Software <development@itextpdf.com>
2023-04-26 18:10:34 +0000

    [RELEASE] pdfHTML 5.0.0

--- itext/itext.html2pdf/Properties/AssemblyInfo.cs
+++ itext/itext.html2pdf/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using Versions.Attributes;
 
 [assembly: AssemblyVersion("5.0.0.0")]
 [assembly: AssemblyFileVersion("5.0.0.0")]
-[assembly: AssemblyInformationalVersion("5.0.0-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("5.0.0")]
