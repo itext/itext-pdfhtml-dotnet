576e9ea896a1c61db60769fd91c323a3665a733b
Nanou Persoons <nanou.persoons@apryse.com>
2023-06-22 08:59:59 +0000

    Go through all W3C test files and add tests
    DEVSIX-7590
    
    Autoported commit.
    Original commit hash: [646d397eb]

---
+++ itext.tests/itext.html2pdf.tests/itext/html2pdf/css/w3c/css_flexbox/FlexboxCollapsedItemHoriz002Test.cs
@@ -0,0 +1,37 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2023 Apryse Group NV
+Authors: Apryse Software.
+
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using System;
+using iText.Html2pdf.Css.W3c;
+using iText.Html2pdf.Logs;
+using iText.Test.Attributes;
+
+namespace iText.Html2pdf.Css.W3c.Css_flexbox {
+    //TODO DEVSIX-5196 change after collapse is supported on flex items
+    //TODO DEVSIX-5164 change after align-content: flex-start is supported
+    [LogMessage(Html2PdfLogMessageConstant.FLEX_PROPERTY_IS_NOT_SUPPORTED_YET, Count = 7)]
+    public class FlexboxCollapsedItemHoriz002Test : W3CCssTest {
+        protected internal override String GetHtmlFileName() {
+            return "flexbox-collapsed-item-horiz-002.html";
+        }
+    }
+}
