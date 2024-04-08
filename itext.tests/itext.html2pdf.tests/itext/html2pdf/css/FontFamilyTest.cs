a03807ddfaab764e9dfe608211abc0d7e2c311af
Dmitry Chubrick <dmitry.chubrick@apryse.com>
2024-04-08 09:07:17 +0000

    Fix possible ReDos vulnerability in FontfamilySplitterUtil
    DEVSIX-8227
    
    Autoported commit.
    Original commit hash: [92a8fa24d]

---
+++ itext.tests/itext.html2pdf.tests/itext/html2pdf/css/FontFamilyTest.cs
@@ -0,0 +1,53 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2024 Apryse Group NV
+Authors: Apryse Software.
+
+This program is offered under a commercial and under the AGPL license.
+For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+AGPL licensing:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using System;
+using System.IO;
+using iText.Html2pdf;
+using iText.Kernel.Utils;
+using iText.Test;
+
+namespace iText.Html2pdf.Css {
+    [NUnit.Framework.Category("IntegrationTest")]
+    public class FontFamilyTest : ExtendedITextTest {
+        public static readonly String SOURCE_FOLDER = iText.Test.TestUtil.GetParentProjectDirectory(NUnit.Framework.TestContext
+            .CurrentContext.TestDirectory) + "/resources/itext/html2pdf/css/FontFamilyTest/";
+
+        public static readonly String DESTINATION_FOLDER = NUnit.Framework.TestContext.CurrentContext.TestDirectory
+             + "/test/itext/html2pdf/css/FontFamilyTest/";
+
+        [NUnit.Framework.OneTimeSetUp]
+        public static void BeforeClass() {
+            CreateDestinationFolder(DESTINATION_FOLDER);
+        }
+
+        [NUnit.Framework.Test]
+        public virtual void HugeFontFamilyForDosAttackTest() {
+            String htmlPath = SOURCE_FOLDER + "hugeFontFamilyForDosAttackTest.html";
+            String pdfPath = DESTINATION_FOLDER + "hugeFontFamilyForDosAttackTest.pdf";
+            String cmpPdfPath = SOURCE_FOLDER + "cmp_hugeFontFamilyForDosAttackTest.pdf";
+            HtmlConverter.ConvertToPdf(new FileInfo(htmlPath), new FileInfo(pdfPath));
+            NUnit.Framework.Assert.IsNull(new CompareTool().CompareByContent(pdfPath, cmpPdfPath, DESTINATION_FOLDER, 
+                "diff_"));
+        }
+    }
+}
