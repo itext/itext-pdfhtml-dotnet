418b88a28dd85366756aa5627791e3449c9d8d3b
Apryse Software <development@apryse.com>
2024-07-12 15:05:03 +0000

    [RELEASE] pdfHtml 5.0.5

--- itext/itext.html2pdf/Properties/AssemblyInfo.cs
+++ itext/itext.html2pdf/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using Versions.Attributes;
 
 [assembly: AssemblyVersion("5.0.5.0")]
 [assembly: AssemblyFileVersion("5.0.5.0")]
-[assembly: AssemblyInformationalVersion("5.0.5-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("5.0.5")]
